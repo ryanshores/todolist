f5ec361ab1c97e5606a9f0f1a93335fce0afca6b
Ryan Shores <ryan.shores@me.com>
2018-06-13 22:50:25 -0500

    Add in add todo from index view

--- public/app.js
+++ public/app.js
@@ -1,15 +1,43 @@
 $(document).ready(function(){
+	
 	$.getJSON("/api/todos")
 	.then(addTodos)
 	.catch(function(err){
 		console.log(err);
-	})
+	});
+
+	$("#todoInput").keypress(function(event){
+		if(event.which == 13){
+			// create todo
+			createTodo();
+		}
+	});
 });
 
 function addTodos(todos){
 	// add todos to page here
 	todos.forEach(function(todo){
-		var newTodo = $("<li class='task'>" + todo.name + "</li>");
-		$(".list").append(newTodo);
+		addTodo(todo);
+	});
+}
+
+function addTodo(todo){
+	var newTodo = $("<li class='task'>" + todo.name + "</li>");
+	if(todo.completed){
+		newTodo.addClass("done");
+	}
+	$(".list").append(newTodo);
+}
+
+function createTodo(){
+	// send request to create new todo
+	var userInput = $("#todoInput").val();
+	$.post("/api/todos", {name: userInput})
+	.then(function(newTodo){
+		addTodo(newTodo);
+		$("#todoInput").val("")
+	})
+	.catch(function(err){
+		console.log(err);
 	});
 }
